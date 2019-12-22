8138ab40e9dd1768b7ed8ab10bfbcfc0c4558dd4
Helio <helios25@gmail.com>
2019-12-22 11:48:14 -0500

    updated razzle config to stop using tslint and applied formatting based on eslint settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './Home';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
 
-import './App.css';
+import "./App.css";
 
 const App = () => (
   <Switch>
--- src/server.tsx
+++ src/server.tsx
@@ -1,9 +1,9 @@
-import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
+import express from "express";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
 
-import App from './App';
+import App from "./App";
 
 let assets: any;
 
@@ -13,14 +13,14 @@ const syncLoadAssets = () => {
 syncLoadAssets();
 
 const server = express()
-  .disable('x-powered-by')
+  .disable("x-powered-by")
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR!))
-  .get('/*', (req: express.Request, res: express.Response) => {
+  .get("/*", (req: express.Request, res: express.Response) => {
     const context = {};
     const markup = renderToString(
       <StaticRouter context={context} location={req.url}>
         <App />
-      </StaticRouter>
+      </StaticRouter>,
     );
     res.send(
       `<!doctype html>
@@ -30,21 +30,17 @@ const server = express()
         <meta charSet='utf-8' />
         <title>Razzle TypeScript</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-        assets.client.css
-          ? `<link rel="stylesheet" href="${assets.client.css}">`
-          : ''
-      }
+        ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ""}
           ${
-        process.env.NODE_ENV === 'production'
-          ? `<script src="${assets.client.js}" defer></script>`
-          : `<script src="${assets.client.js}" defer crossorigin></script>`
-      }
+            process.env.NODE_ENV === "production"
+              ? `<script src="${assets.client.js}" defer></script>`
+              : `<script src="${assets.client.js}" defer crossorigin></script>`
+          }
     </head>
     <body>
         <div id="root">${markup}</div>
     </body>
-</html>`
+</html>`,
     );
   });
 
