f2f32b58853705ff45c55d5caf9f60c98f2759fe
Helio <helios25@gmail.com>
2019-12-22 11:33:40 -0500

    installed missing package and added eslint and prettier configuration files

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
+import React from "react";
 
-class Home extends React.Component<{}, {}> {
-  public render() {
-    return (
-      <div className="Home">
-        <p className="Home-intro">
-          To get started, edit <code>src/App.tsx</code> or{' '}
-          <code>src/Home.tsx</code> and save to reload.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+import Button from "./components/Button";
+
+const Home = () => (
+  <div>
+    <p>
+      To get started, edit <code>src/App.tsx</code> or <code>src/Home.tsx</code> and save to reload.
+    </p>
+    <ul>
+      <li>
+        <a href="https://github.com/jaredpalmer/razzle">Docs</a>
+      </li>
+      <li>
+        <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
+      </li>
+      <li>
+        <a href="https://palmer.chat">Community Slack</a>
+      </li>
+    </ul>
+    <Button>Button</Button>
+  </div>
+);
 
 export default Home;
