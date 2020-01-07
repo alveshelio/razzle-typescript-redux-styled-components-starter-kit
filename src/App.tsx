771115402bafadc93179ebfe3745bd1a000b8b0e
Helio <helios25@gmail.com>
2020-01-06 23:14:56 -0500

    fixed issues with module hot reload

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 
-import "./App.css";
-
 const App = () => (
   <Switch>
     <Route exact={true} path="/" component={Home} />
